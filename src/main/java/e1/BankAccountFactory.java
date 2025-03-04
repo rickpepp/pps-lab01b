2b37a73ab42e5b0bed990c52a98ad8fc83dc5007
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2025-03-04 13:54:01 +0100

    Deleted Magic Number from BankAccountFactory

--- src/main/java/e1/BankAccountFactory.java
+++ src/main/java/e1/BankAccountFactory.java
@@ -1,9 +1,15 @@
 package e1;
 
 public class BankAccountFactory {
+
+    public static final int GOLD_ACCOUNT_OVERDRAFT = 500;
+    public static final int FEE_BRONZE_AND_SILVER = 1;
+    public static final int WITHDRAW_LIMIT_NO_FEE_ACCOUNT_BRONZE = 100;
+
     public BankAccount createSilverBankAccount() {
         BankAccount coreAccount = createBasicAccount();
-        coreAccount = new WithdrawFunctionFeeDecorator(coreAccount, amountWithdraw -> 1);
+        coreAccount = new WithdrawFunctionFeeDecorator(coreAccount,
+                amountWithdraw -> FEE_BRONZE_AND_SILVER);
         coreAccount = new OverDraftDecorator(coreAccount, 0);
         return coreAccount;
     }
@@ -16,14 +22,14 @@ public class BankAccountFactory {
 
     public BankAccount createGoldBankAccount() {
         BankAccount coreAccount = createBasicAccount();
-        coreAccount = new OverDraftDecorator(coreAccount, 500);
+        coreAccount = new OverDraftDecorator(coreAccount, GOLD_ACCOUNT_OVERDRAFT);
         return coreAccount;
     }
 
     public BankAccount createBronzeBankAccount() {
         BankAccount coreAccount = createBasicAccount();
         coreAccount = new WithdrawFunctionFeeDecorator(coreAccount,
-                amountWithdraw -> (amountWithdraw < 100) ? 0 : 1);
+                amountWithdraw -> (amountWithdraw < WITHDRAW_LIMIT_NO_FEE_ACCOUNT_BRONZE) ? 0 : FEE_BRONZE_AND_SILVER);
         coreAccount = new OverDraftDecorator(coreAccount, 0);
         return coreAccount;
     }
