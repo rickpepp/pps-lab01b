a1a47692b978f5b5c4235371cd7cea020a2c58e4
Pepe Riccardo <riccardo.pepe3@studio.unibo.it>
2025-03-06 16:02:29 +0100

    Refactored LogicImpl with new ChessPiece Solution

--- src/main/java/e2/LogicsImpl.java
+++ src/main/java/e2/LogicsImpl.java
@@ -3,32 +3,51 @@ package e2;
 import java.util.*;
 
 public class LogicsImpl implements Logics {
-	
-	private final Pair<Integer,Integer> pawn;
-	private Pair<Integer,Integer> knight;
+
+	private ChessPiece knight;
+	private ChessPiece pawn;
 	private final Random random = new Random();
 	private final int size;
 
-	public LogicsImpl(int size, Pair<Integer,Integer> knight, Pair<Integer,Integer> pawn) {
-		checkValidSize(size);
-		this.size = size;
-		checkValidPosition(pawn);
-		this.pawn = pawn;
-		checkValidPosition(knight);
-		this.knight = knight;
+	public LogicsImpl(int size, Pair<Integer,Integer> knightPosition, Pair<Integer,Integer> pawnPosition) {
+		this(size);
+		checkValidPosition(pawnPosition);
+		checkValidPosition(knightPosition);
+		this.pawn.setNewPosition(pawnPosition);
+		this.knight.setNewPosition(knightPosition);
 	}
 
-	private void checkValidPosition(Pair<Integer,Integer> position) {
-		if (position.getX() < 0 || position.getY() < 0 || position.getX() >= this.size || position.getY() >= this.size)
-			throw new IndexOutOfBoundsException();
-	}
-
-    public LogicsImpl(int size){
+	public LogicsImpl(int size){
 		checkValidSize(size);
 		this.size = size;
-        this.pawn = this.randomEmptyPosition();
-        this.knight = this.randomEmptyPosition();
-    }
+		createPawnAndKnight();
+		this.pawn.setNewPosition(this.randomEmptyPosition());
+		this.knight.setNewPosition(this.randomEmptyPosition());
+	}
+    
+	@Override
+	public boolean hit(int row, int col) {
+		checkValidPosition(new Pair<>(row, col));
+		if (this.knight.getPossibleMovements(this.size).contains(new Pair<>(row, col)))
+			this.knight.setNewPosition(new Pair<>(row, col));
+		return this.knight.getPosition().equals(this.pawn.getPosition());
+	}
+
+	@Override
+	public boolean hasKnight(int row, int col) {
+		return this.knight.getPosition().equals(new Pair<>(row,col));
+	}
+
+	@Override
+	public boolean hasPawn(int row, int col) {
+		return this.pawn.getPosition().equals(new Pair<>(row,col));
+	}
+
+	private void createPawnAndKnight() {
+		ChessPieceFactory factory = new ChessPieceFactory();
+		this.pawn = factory.createPawn();
+		this.knight = factory.createKnight();
+	}
 
 	private void checkValidSize(int size) {
 		if (size <= 1)
@@ -36,33 +55,13 @@ public class LogicsImpl implements Logics {
 	}
 
 	private Pair<Integer,Integer> randomEmptyPosition(){
-    	Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
-    	// the recursive call below prevents clash with an existing pawn
-    	return this.pawn!=null && this.pawn.equals(pos) ? randomEmptyPosition() : pos;
-    }
-    
-	@Override
-	public boolean hit(int row, int col) {
-		if (row<0 || col<0 || row >= this.size || col >= this.size) {
+		Pair<Integer,Integer> pos = new Pair<>(this.random.nextInt(size),this.random.nextInt(size));
+		// the recursive call below prevents clash with an existing pawn
+		return this.pawn!=null && this.pawn.equals(pos) ? randomEmptyPosition() : pos;
+	}
+
+	private void checkValidPosition(Pair<Integer,Integer> position) {
+		if (position.getX() < 0 || position.getY() < 0 || position.getX() >= this.size || position.getY() >= this.size)
 			throw new IndexOutOfBoundsException();
-		}
-		// Below a compact way to express allowed moves for the knight
-		int x = row-this.knight.getX();
-		int y = col-this.knight.getY();
-		if (x!=0 && y!=0 && Math.abs(x)+Math.abs(y)==3) {
-			this.knight = new Pair<>(row,col);
-			return this.pawn.equals(this.knight);
-		}
-		return false;
-	}
-
-	@Override
-	public boolean hasKnight(int row, int col) {
-		return this.knight.equals(new Pair<>(row,col));
-	}
-
-	@Override
-	public boolean hasPawn(int row, int col) {
-		return this.pawn.equals(new Pair<>(row,col));
 	}
 }
